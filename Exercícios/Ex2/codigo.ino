963383c30122eb0d72f9e1e54a10d1adcc8f0a38
pinezi0205 <78920618+pinezi0205@users.noreply.github.com>
2022-03-21 09:38:31 -0300

    Update codigo.ino

--- Exercícios/Ex2/codigo.ino
+++ Exercícios/Ex2/codigo.ino
@@ -6,13 +6,11 @@ void setup()
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  delay(1000); 
-  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); 
+  digitalWrite(3, LOW);
   delay(1000);
   
   digitalWrite(3, HIGH);
-  delay(1000); 
-  digitalWrite( 3, LOW);
+  digitalWrite(2, LOW);
   delay(1000); 
 }
